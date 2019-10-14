36716ec2510a45b7902bbae44c99db1d7d491dc7
Nathan Pickard <nathan.pickard87@gmail.com>
2019-10-14 12:50:32 -0700

    className and comments added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import ProductList from './components/products';
+import ProductList from './components/products'; // Imports the product list to be rendered
 import './App.css';
 
 class App extends Component {
 
+  // State to store the output from the API request
   state = {
-    products: []
+    products: [],
+    page: 1
   }
 
   componentDidMount() {
-    fetch('https://api.bestbuy.com/v1/products(search=oven&search=stainless&search=steel)?format=json&show=all&apiKey=mPlbr5GXMVkagVgzwT7T2V5X')
-      .then(res => res.json())
+    // Method to be called immediately after the component is mounted
+    this.loadProducts();
+  }
+
+  loadProducts = () => {
+    const { page } = this.state; // Passing in the first page of products
+    //  URL to fetch request from API
+    const url = `https://api.bestbuy.com/v1/products(search=oven&search=stainless&search=steel)?page=${page}&format=json&show=all&apiKey=mPlbr5GXMVkagVgzwT7T2V5X`
+    fetch(url)
+      .then(res => res.json()) // Parse the output to JSON
       .then((data) => {
-        this.setState({ products: data.products })
-        console.log(data.products[0].images);
+        this.setState({ products: data.products }) // Sets the value of the state to the outout from the API
       })
-      .catch(console.log)
+      .catch(console.log) // Log any errors
   }
 
   render() {
     return (
+      // Passing in the product products component, along with the state to be rendered
       <ProductList products={this.state.products} />
     );
   }
--- src/components/products.js
+++ src/components/products.js
@@ -1,20 +1,22 @@
 import React from 'react'
 
 const ProductList = ({ products }) => {
+  // Returns the mapped version of the products state
   return (
     <div>
       <center><h1>Product List</h1></center>
-      <div class="row">
+      <div className="row">
+        {/* // Loops over the products state to insert the specific products name, image, description and price */}
         {products.map((product, i) => (
           // Bootstrap classes for responsive design on various screen sizes
-          <div class="col-xs-12 col-sm-6 col-xl-4">
-            <div key={i} class="card text-center">
-              <div class="card-body">
-                <h5 class="card-title">{product.name}</h5>
+          <div key={i} className="col-xs-12 col-sm-6 col-xl-4">
+            <div className="card text-center">
+              <div className="card-header">{product.name}</div>
+              <div className="card-body">
                 <img src={product.images[0].href} alt="card image" />
-                <h6 class="card-text">Description:</h6>
-                <p class="card-text">{product.shortDescription}</p>
-                <p class="card-text">Price: ${product.salePrice}</p>
+                <h6 className="card-text">Description:</h6>
+                <p className="card-text">{product.shortDescription}</p>
+                <p className="card-text">Price: ${product.salePrice}</p>
               </div>
             </div>
           </div>
